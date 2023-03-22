35144d32a29aa142799e41b45a0f56a7bd92a131
Maria Mutiloa <maria.mutiloa@opendeusto.es>
2023-03-22 17:13:32 +0100

    creado metodos para saber el nombre del usuario que hace el log in

---
+++ bd/data/getData.c
@@ -0,0 +1,66 @@
+#include "./sqlite/sqlite3.h"
+#include "conexion.h"
+#include <stdio.h>
+#include "getData.h"
+#include <string.h>
+#include <stdlib.h>
+
+char * getCliente (char * dni){
+    int rc;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+    char * nombre;
+    nombre = (char *) malloc (15* sizeof(char));
+
+    char * sql = "SELECT *FROM CLIENTE WHERE " dni = ?"";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &res,0);
+
+    if(rc == SQLITE_OK){
+        sqlite3_bind_text(res, 1, dni, sizeof(*dni), SQLITE_STATIC);
+    }
+    else{
+        fprintf(stderr,"Failed to execute statement: %s\n", sqlite3_errmsg(db));
+    }
+
+    int step = sqlite3_step(res);
+
+    if(step == SQLITE_ROW){
+        strcpy(nombre, (const char *) sqlite3_column_text(res,0));
+    }
+
+    else{
+        printf("No results foudn")
+    }
+    return nombre;
+}
+
+char * getAdministrador (char * dni){
+    int rc;
+    char *err_msg = 0;
+    sqlite3_stmt *res;
+    char * nombre;
+    nombre = (char *) malloc (15* sizeof(char));
+
+    char * sql = "SELECT *FROM ADMINISTRADOR WHERE " dni = ?"";
+
+    rc = sqlite3_prepare_v2(db, sql, -1, &res,0);
+
+    if(rc == SQLITE_OK){
+        sqlite3_bind_text(res, 1, dni, sizeof(*dni), SQLITE_STATIC);
+    }
+    else{
+        fprintf(stderr,"Failed to execute statement: %s\n", sqlite3_errmsg(db));
+    }
+
+    int step = sqlite3_step(res);
+
+    if(step == SQLITE_ROW){
+        strcpy(nombre, (const char *) sqlite3_column_text(res,0));
+    }
+
+    else{
+        printf("No results foudn")
+    }
+    return nombre;
+}
---
+++ bd/data/getData.h
@@ -0,0 +1,7 @@
+#define GETDATA_H
+#ifndef GETDATA_H
+
+char * getCliente (char * dni);
+char * getAdministrador (char * dni);
+
+#endif
