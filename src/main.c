f187b5334567a74c060407f009441b27709c8e92
Maria Mutiloa <maria.mutiloa@opendeusto.es>
2023-04-01 16:27:21 +0200

    insert de productos hecho

--- src/main.c
+++ src/main.c
@@ -5,9 +5,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 sqlite3 *db;
 
 int main(){
+     startConn();
+
+    // Insertar 20 productos básicos en la tabla "productos"
+    char *productos[20][3] = {
+        {"1.5", "1", "Leche"},
+        {"2", "2", "Pan"},
+        {"2.5", "3", "Huevos"},
+        {"1.7", "4", "Arroz"},
+        {"3.4", "5", "Berenjena"},
+        {"4.5", "6", "Pollo"},
+        {"7.9", "7", "Merluza"},
+        {"8", "8", "Picadillo de ternera"},
+        {"2.3", "9", "Cebolla"},
+        {"3.5", "10", "Tomate"},
+        {"5.6", "11", "Patata"},
+        {"1.25", "12", "Zanahoria"},
+        {"2", "13", "Lechuga"},
+        {"3", "14", "Pimiento"},
+        {"1", "15", "Ajo"},
+        {"2", "16", "Plátano"},
+        {"2", "17", "Manzana"},
+        {"3", "18", "Naranja"},
+        {"4", "19", "Melón"},
+        {"3", "20", "Sandía"}
+    };
+
+    for(int i = 0; i < 20; i++){
+        char *importe = productos[i][0];
+        char *cod_prod = productos[i][1];
+        char *descripcion = productos[i][2];
+
+        char sql[128];
+        sprintf(sql, "INSERT INTO productos (importe, cod_prod, descripcion) VALUES (%s, %s, '%s');", importe, cod_prod, descripcion);
+
+        int rc = sqlite3_exec(db, sql, 0, 0, 0);
+        if(rc != SQLITE_OK){
+            fprintf(stderr, "SQL error: %s\n", sqlite3_errmsg(db));
+            return 1;
+        }
+    }
+
+    sqlite3_close(db);
+
     startConn();
     
     char dni[20], contrasena[20];
