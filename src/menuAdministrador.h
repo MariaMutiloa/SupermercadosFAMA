de4e5fd7dbe61645960940e981c5f2487715ad96
Adrian Martinez <adrianmartinez@opendeusto.es>
2023-04-03 12:05:17 +0200

    arreglados los .h

--- src/menuAdministrador.h
+++ src/menuAdministrador.h
@@ -1,2 +1,7 @@
 #include "../bd/data/getData.h"
+#ifndef MENUADMINISTRADOR_H_
+#define MENUADMINISTRADOR_H_
+
 void admin_menu();
+
+#endif
--- src/menuCliente.h
+++ src/menuCliente.h
@@ -1,1 +1,7 @@
+#ifndef MENUCLIENTE_H_
+#define MENUCLIENTE_H_
+
 void menu_cliente();
+
+#endif
+
